ff7047aedd966e697482cb4b006c820523a63d62
Yashraj Jhala <jhalayashraj@gmail.com>
2017-12-13 19:26:42 +0530

    Added Profile Picture
    - Used paperclip
    - Change Sidebar profile pic and name according to current_user
    - Added Navigation Pages
    - Used new icons for User and Settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   layout :ensure_layout
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def after_sign_out_path_for(resource_or_scope)
     new_session_path(resource_name)
   end
 
   def ensure_layout
     current_user.present? ? 'application' : 'login'
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :avatar, :avatar_cache) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :avatar) }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
          :omniauthable, :omniauth_providers => [:facebook]
 
   ## Paperclip
+  has_attached_file :avatar, styles: { medium: "300x300", thumb: "100x100" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_attachment_file_name :avatar, matches: [/png\Z/, /jpe?g\Z/]
+  do_not_validate_attachment_file_type :avatar
 
   ## Validations
   validates :name, presence: true, length: { maximum: 50 }
