7aa344a2a42ef2185ba74b47b6ada9017479aa2e
Yashraj Jhala <jhalayashraj@gmail.com>
2018-01-23 20:15:55 +0530

    Added Twitter feed to homepage
    - Show tweets of current_user
    - Added links to tweet

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,8 +1,14 @@
-require 'twitter'
+class TwitterApi
+  def self.our_public_tweets
+    client.user_timeline(@current_user, count: 2, exclude_replies: true, include_rts: true)
+  end
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "ptmkRX5UgWQ6N1tyaWVyuSpbt"
-  config.consumer_secret     = "0yr9iJHxl2rOVJJhDvOHoPRW5i7fCIueo3uhiWh01ADw36ShI6"
-  config.access_token        = "379670699-niKcWhvcLkcJrsfAcAZYSCJlgAGZq8m6pfHABtjI"
-  config.access_token_secret = "z9eddjisA7qRinHhELSPthT1u0VIovmLOH3OwbZTz6v01"
+  def self.client
+    @client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key        = "ptmkRX5UgWQ6N1tyaWVyuSpbt"
+      config.consumer_secret     = "0yr9iJHxl2rOVJJhDvOHoPRW5i7fCIueo3uhiWh01ADw36ShI6"
+      config.access_token        = "379670699-niKcWhvcLkcJrsfAcAZYSCJlgAGZq8m6pfHABtjI"
+      config.access_token_secret = "z9eddjisA7qRinHhELSPthT1u0VIovmLOH3OwbZTz6v01"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,4 @@ Rails.application.routes.draw do
   resources :todos
   root :to =>'todos#index'
   match 'todos/:id/toggle_status', to: 'todos#toggle_status', via: 'get'
-  get "home", to: "home#index", as: "user_root"
-  root 'home#index'
 end
